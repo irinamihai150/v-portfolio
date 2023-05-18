b94ccd1d4e65b9e61334eafe5e1aaf5556a81344
irinamihai150 <irinamihai50@gmail.com>
2023-05-18 21:28:10 +0100

    fix

--- src/assets/Contact.jsx
+++ src/assets/Contact.jsx
@@ -3,10 +3,12 @@ import { FaGithub, FaLinkedin } from "react-icons/fa"
 function Contact() {
 	return (
 		<div>
-			<section id='#contact'>
+			<section id='contact'>
 				<h2 className='text-5xl text-center mb-5 mt-4'>CONTACT ME </h2>
 				<div className='flex items-center justify-center mb-12'>
-					<FaGithub className='text-yellow-400 w-40 h-10' />
+					<a href='https://github.com/irinamihai150' rel='noopener noreferrer'>
+						<FaGithub className='text-yellow-400 w-40 h-10' />
+					</a>
 
 					<a
 						href='https://www.linkedin.com/in/irinamih/'
